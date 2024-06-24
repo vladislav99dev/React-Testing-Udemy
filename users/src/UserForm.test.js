6b962acaef113201b0ca84420508521c64ea1024
Vladislav Dorovski <Vladislav.Dorovski@a1.bg>
2024-06-24 21:48:07 +0300

    add another test

--- users/src/UserForm.test.js
+++ users/src/UserForm.test.js
@@ -11,3 +11,25 @@ test("it shows two inputs and a button", () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test("it calls onUserAdd when the form is submitted", async () => {
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  render(<UserForm onUserAdd={callback} />);
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  await user.click(emailInput);
+  await user.keyboard("jane@jane.com");
+
+  await user.click(nameInput);
+  await user.keyboard("jane");
+
+  const button = screen.getByRole("button");
+  await user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: "jane", email: "jane@jane.com" });
+});
