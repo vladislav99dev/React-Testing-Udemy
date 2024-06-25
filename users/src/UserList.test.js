c3c854904c08a29d1c95cc7142da5d652b4067ba
vladislav99dev <74834080+vladislav99dev@users.noreply.github.com>
2024-06-25 20:47:12 +0300

    introduce data-testid

--- users/src/UserList.test.js
+++ users/src/UserList.test.js
@@ -1,10 +1,17 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, within } from "@testing-library/react";
 import UserList from "./UserList";
 
 test("new test", () => {
-  render(<UserList users={[{ name: "Vladislav", email: "Dorovski" }]} />);
+  render(
+    <UserList
+      users={[
+        { name: "Vladislav", email: "Dorovski" },
+        { name: "Vladislavc", email: "Dorovski" },
+      ]}
+    />
+  );
 
-  const rows = screen.getAllByRole("row");
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   expect(rows).toHaveLength(2);
 });
