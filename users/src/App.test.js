59b1e283ef066d4bf74bb39a5a0aed4716e4c78e
vladislav99dev <74834080+vladislav99dev@users.noreply.github.com>
2024-06-26 10:25:00 +0300

    test App.js

--- users/src/App.test.js
+++ users/src/App.test.js
@@ -1,6 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("can receive a new user and show it on a list", async () => {
   render(<App />);
+
+  const [nameInput, emailInput] = [
+    screen.getByRole("textbox", { name: /name/i }),
+    screen.getByRole("textbox", { name: /email/i }),
+  ];
+
+  const button = screen.getByRole("button");
+
+  await user.click(nameInput);
+  await user.keyboard("jane");
+
+  await user.click(emailInput);
+  await user.keyboard("jane@jane.com");
+
+  await user.click(button);
+
+  const name = screen.getByRole("cell", { name: "jane" });
+  const email = screen.getByRole("cell", { name: "jane@jane.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
