516caf53c263ad9d299189da0b9a194bc04159fa
Vladislav Dorovski <74834080+vladislav99dev@users.noreply.github.com.>
2024-06-27 21:38:03 +0300

    add exercise to find els by role

---
+++ users/src/Exercise.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Exercise = () => {
+    return (
+        <div>
+            <a href="/">Link</a>
+            <button>Button</button>
+            <footer>ContentInfo</footer>
+            <h1>Heading</h1>
+            <header>Banner</header>
+            <img alt="description" />
+            <input type="checkbox" />
+            <input type="number" />
+            <input type="radio" />
+            <input type="text" />
+            <li>Listitem</li>
+            <ul>Listgroup</ul>
+        </div>
+    )
+}
+
+export default Exercise
